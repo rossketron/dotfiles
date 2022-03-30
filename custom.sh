645b8f835cc7a07b027ebbcef7ee8a13ac1eac73
Ross Ketron <rossketron@gmail.com>
2022-03-30 17:47:38 -0400

    feat: Add aliases for grepping from history and files

--- custom.sh
+++ custom.sh
@@ -5,3 +5,11 @@ alias ssh-linode="ssh ross@139.177.205.136"
 alias ssh-tellico="ssh cketron2@tellico.icl.utk.edu"
 alias ssh-hydra="ssh cketron2@hydra22.eecs.utk.edu"
 
+grab() {
+    history | grep $1
+}
+
+grabf() {
+    cat $1 | grep $2
+}
+
